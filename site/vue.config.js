1c4861e9ed481dad1961ff091f708af258a89484
Tales Moreira <tales.info@gmail.com>
2019-03-29 18:43:03 -0300

    primeira versao

--- site/vue.config.js
+++ site/vue.config.js
@@ -1,17 +1,18 @@
 // vue.config.js
 module.exports = {
-    transpileDependencies: [
-        'vue-echarts',
-        'resize-detector'
-    ],
-    css: {
-      loaderOptions: {
-        // pass options to sass-loader
-        sass: {
-          // @/ is an alias to src/
-          // so this assumes you have a file named `src/variables.scss`
-          data: `@import "@/assets/variaveis.scss";`
-        }
+  publicPath: process.env.NODE_ENV === 'production' ? '/AlumniPsiUFMG' : '/',
+  transpileDependencies: [
+    'vue-echarts',
+    'resize-detector'
+  ],
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "@/assets/variaveis.scss";`
       }
     }
   }
+}
