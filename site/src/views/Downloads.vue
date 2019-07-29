056b4d3c73d9274406196c6680186eb6510eb855
Tales Moreira <tales.info@gmail.com>
2019-07-29 08:22:39 -0300

    Final

--- site/src/views/Downloads.vue
+++ site/src/views/Downloads.vue
@@ -3,15 +3,15 @@
         <cabecalho titulo="Downloads"/>
         <div class="container mb-4">
             <ul class="list-group">
-                <li class="list-group-item"><a target="blank" href="/downloads/Egressos de Doutorado e Orientadores.docx">Egressos de Doutorado e Orientadores</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Instituições de Colaboração em Minas Gerais.docx">Instituições de Colaboração em Minas Gerais</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Instituições de Colaboração.docx">Instituições de Colaboração</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Instituições Editoras dos Periodicos.docx">Instituições Editoras dos Periodicos</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Lista Autores.docx">Lista Autores</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Periodicos em Ordem Alfabetica.docx">Periodicos em Ordem Alfabetica</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Periódicos Estrangeiros.docx">Periódicos Estrangeiros</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Periódicos Ordem por Quantidade Artigos.docx">Periódicos Ordem por Quantidade Artigos</a></li>
-                <li class="list-group-item"><a target="blank" href="/downloads/Titulados Doutorado Psicologia.pdf">Titulados Doutorado Psicologia</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Egressos de Doutorado e Orientadores.docx">Egressos de Doutorado e Orientadores</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Instituições de Colaboração em Minas Gerais.docx">Instituições de Colaboração em Minas Gerais</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Instituições de Colaboração.docx">Instituições de Colaboração</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Instituições Editoras dos Periodicos.docx">Instituições Editoras dos Periodicos</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Lista Autores.docx">Lista Autores</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Periodicos em Ordem Alfabetica.docx">Periodicos em Ordem Alfabetica</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Periódicos Estrangeiros.docx">Periódicos Estrangeiros</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Periódicos Ordem por Quantidade Artigos.docx">Periódicos Ordem por Quantidade Artigos</a></li>
+                <li class="list-group-item"><a target="blank" href="/AlumniPsiUFMG/downloads/Titulados Doutorado Psicologia.pdf">Titulados Doutorado Psicologia</a></li>
             </ul>
                
         </div>
