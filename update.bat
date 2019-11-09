19acd5dd42bb2cfc56495ba02e883302f5cb005b
Tales Moreira <tales.info@gmail.com>
2019-11-09 10:18:06 -0300

    teste

--- update.bat
+++ update.bat
@@ -1,5 +1,7 @@
+set message=%1
+if "%1"=="" set message="atualizacao"
 git checkout master
 git add .
-git commit -m "%1"
+git commit -m "%message%"
 git push origin master
 git subtree push --prefix site/dist origin gh-pages
